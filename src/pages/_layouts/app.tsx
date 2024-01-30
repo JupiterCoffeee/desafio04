4cafbea73e3e16f8fea00e5369680a42fb8eca30
JupiterCoffeee <jvcfialho@gmail.com>
2024-01-30 17:17:37 +0000

    React router dom added and configured Auth and app layouts created

---
+++ src/pages/_layouts/app.tsx
@@ -0,0 +1,15 @@
+import { Outlet } from 'react-router-dom'
+
+export function AppLayout() {
+  return (
+    <>
+      <div>
+        <h1>App</h1>
+      </div>
+
+      <div>
+        <Outlet />
+      </div>
+    </>
+  )
+}
