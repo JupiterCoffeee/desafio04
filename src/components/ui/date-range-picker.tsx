812f5c3a1043743eb586f01646492c2a9800d5ce
JupiterCoffeee <jvcfialho@gmail.com>
2024-03-04 16:51:46 +0000

    Daypicker, calendar and popover shadcnui components added

---
+++ src/components/ui/date-range-picker.tsx
@@ -0,0 +1,45 @@
+import { format } from 'date-fns'
+import { Calendar as CalendarIcon } from 'lucide-react'
+import * as React from 'react'
+import { DateRange } from 'react-day-picker'
+
+import { Button } from '@/components/ui/button'
+import { Calendar } from '@/components/ui/calendar'
+import {
+  Popover,
+  PopoverContent,
+  PopoverTrigger,
+} from '@/components/ui/popover'
+import { cn } from '@/lib/utils'
+
+interface DateRangePickerProps extends React.ComponentProps<'div'> {
+  date: Date | undefined
+  onDateChange: (date: DateRange) => void
+}
+
+export function DateRangePicker({ date, onDateChange }: DateRangePickerProps) {
+  return (
+    <Popover>
+      <PopoverTrigger asChild>
+        <Button
+          variant={'outline'}
+          className={cn(
+            'w-[280px] justify-start text-left font-normal',
+            !date && 'text-muted-foreground',
+          )}
+        >
+          <CalendarIcon className="mr-2 h-4 w-4" />
+          {date ? format(date, 'PPP') : <span>Pick a date</span>}
+        </Button>
+      </PopoverTrigger>
+      <PopoverContent className="w-auto p-0">
+        <Calendar
+          mode="single"
+          selected={date || undefined}
+          onSelect={() => onDateChange}
+          initialFocus
+        />
+      </PopoverContent>
+    </Popover>
+  )
+}
